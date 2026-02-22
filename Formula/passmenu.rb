867a3a53def27c0e817e1ef64bd1771589cbd64f
ub3 <umarbatalov@gmail.com>
2026-02-23 01:15:36 +0300

    passmenu

--- Formula/passmenu.rb
+++ Formula/passmenu.rb
@@ -1,5 +1,5 @@
 class Passmenu < Formula
-  desc "A popup menu for macOS / Darwin using fzf to search and paste pass passwords easily."
+  desc "macOS popup menu to search for pass passwords"
   homepage "https://github.com/ub-3/passmenu"
   url "https://github.com/ub-3/passmenu/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "6f9c3319cb01f82859c487185b014dfa72b97e20a5fbd7cb01f781123085dac3"
