7e21a9e76a105e8d487ae957a1f78c7b74c13311
uabatalov <uabatalov@avito.ru>
2026-06-30 13:29:21 +0300

    passmenu

--- Formula/passmenu.rb
+++ Formula/passmenu.rb
@@ -4,6 +4,9 @@ class Passmenu < Formula
   url "https://github.com/ub-3/passmenu/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "6f9c3319cb01f82859c487185b014dfa72b97e20a5fbd7cb01f781123085dac3"
   license "MIT"
+  revision 1
+
+  depends_on "fzf"
 
   def install
     bin.install "passmenu" => "passmenu"
